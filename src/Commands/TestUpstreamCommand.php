fe78a50301fba4e0236ff24b0ae367b9a47fd2ce
Paul Hendrick <paul.hendrick@communitybrands.com>
2019-11-07 17:39:33 -0500

    Add a registry rebuild step

--- src/Commands/TestUpstreamCommand.php
+++ src/Commands/TestUpstreamCommand.php
@@ -194,6 +194,8 @@ class TestUpstreamCommand extends TerminusCommand implements SiteAwareInterface
             $this->log()->notice($workflow->getMessage());
         }
 
+        $this->log()->notice('Rebuilding registry and system tables (with Drush)');
+        $this->sendDrushCommand($env, 'rr');
         $this->log()->notice('Running Drush Clear Cache All');
         $this->sendDrushCommand($env, 'cc all');
         $this->log()->notice('Running Drush Updatedb');
